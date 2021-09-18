4e0e025f32e3588dac54fa6a0dc43f02c9716a28
Wagner Lima <wagner.araujo92@outlook.com>
2021-09-18 14:08:19 -0300

    Teste no CRUD e correção do loop no POST

--- src/DevIO.Api/Configuration/DependencyInjectionConfig.cs
+++ src/DevIO.Api/Configuration/DependencyInjectionConfig.cs
@@ -1,4 +1,6 @@
 ﻿using DevIO.Business.Interfaces;
+using DevIO.Business.Notificacoes;
+using DevIO.Business.Services;
 using DevIO.Data.Context;
 using DevIO.Data.Repository;
 using Microsoft.Extensions.DependencyInjection;
@@ -18,8 +20,8 @@ namespace DevIO.Api.Configuration
             services.AddScoped<IFornecedorRepository, FornecedorRepository>();
             services.AddScoped<IEnderecoRepository, EnderecoRepository>();
 
-            //services.AddScoped<INotificador, Notificador>();
-            //services.AddScoped<IFornecedorService, FornecedorService>();
+            services.AddScoped<INotificador, Notificador>();
+            services.AddScoped<IFornecedorService, FornecedorService>();
             //services.AddScoped<IProdutoService, ProdutoService>();
 
             return services;
--- src/DevIO.Api/Startup.cs
+++ src/DevIO.Api/Startup.cs
@@ -39,6 +39,10 @@ namespace DevIO.Api
             services.AddControllers();
 
             services.ResolveDependencies();
+
+            services.AddControllersWithViews()
+                .AddNewtonsoftJson(options =>
+                options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
