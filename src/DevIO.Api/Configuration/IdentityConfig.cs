b66296ca9feee2e050fbc8f0113b152570dd8f16
Wagner Lima <wagner.araujo92@outlook.com>
2021-10-12 11:43:13 -0300

    Identity Mensagens em Portugues

--- src/DevIO.Api/Configuration/IdentityConfig.cs
+++ src/DevIO.Api/Configuration/IdentityConfig.cs
@@ -1,4 +1,5 @@
 ﻿using DevIO.Api.Data;
+using DevIO.Api.Extensions;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
@@ -23,7 +24,7 @@ namespace DevIO.Api.Configuration
             services.AddDefaultIdentity<IdentityUser>()
                     .AddRoles<IdentityRole>()
                     .AddEntityFrameworkStores<ApplicationDbContext>()
-                    //.AddErrorDescriber<IdentityMensagensPortugues>()
+                    .AddErrorDescriber<IdentityMensagensPortugues>() // add mensagens de erro em português
                     .AddDefaultTokenProviders();
 
             return services;
