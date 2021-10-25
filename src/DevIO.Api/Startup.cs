05cf2609fc17356b61a43b6cc1508af065dfb1c7
Wagner Lima <wagner.araujo92@outlook.com>
2021-10-25 11:38:48 -0300

    Documentando a API com Swagger

--- src/DevIO.Api/Startup.cs
+++ src/DevIO.Api/Startup.cs
@@ -1,19 +1,11 @@
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using AutoMapper;
 using DevIO.Api.Configuration;
 using DevIO.Data.Context;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace DevIO.Api
 {
@@ -40,6 +32,8 @@ namespace DevIO.Api
 
             services.AddApiConfig();
 
+            services.AddSwaggerConfig();
+                
             services.ResolveDependencies();
 
             // Ignorar looping Json
@@ -50,10 +44,13 @@ namespace DevIO.Api
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IApiVersionDescriptionProvider provider)
         {
             app.UseAuthentication(); // precisa vir antes da configuração do MVC, se não, não vai funcionar
+
             app.UseApiConfig(env);
+
+            app.UseSwaggerConfig(provider);
         }
     }
 }
